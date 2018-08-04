a6100dbb26ea5ff2f7a0e4d0ff89cca7551154aa
Alex Dong <1029278668@qq.com>
2018-08-04 16:28:20 +0800

    Feature/keyword-pipe (#9)
    * fit tslint
    
    * fit tslint
    
    * add keyword pipe

---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { KeywordPipe } from '../pipes/keyword.pipe';
+
+@NgModule({
+  declarations: [
+    KeywordPipe
+  ],
+  imports: [],
+  exports: [
+    KeywordPipe
+  ]
+})
+export class PipesModule {}
